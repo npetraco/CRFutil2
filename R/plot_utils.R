837f313efad463b09e8c4e4986b995a3162b0feb
npetraco <npetraco@gmail.com>
2025-07-17 15:47:09 -0400

    Remote update

--- R/plot_utils.R
+++ R/plot_utils.R
@@ -2,6 +2,7 @@
 #' @description Plot the sample of states at each node
 #'
 #' @param samples The samples
+#' @param samples a crf object
 #'
 #' @details The function will plot the counts of each state at each node.
 #'
@@ -10,81 +11,7 @@
 #' @examples XXXX
 #'
 #' @export
-plot.marginal.sample <- function(samples, ordered.state.names=NULL) {
-
-  num.nodes <- ncol(samples)
-  num.samps <- nrow(samples)
-
-  node.names  <- colnames(samples)
-  if(is.null(ordered.state.names)){ # If no state names are stipulated:
-    st.lbl.class <- class(samples[1]) # Grab the first state of the first node and get it's class. **NOTE: Assume all the rest of the state labels are the same class.
-    if((st.lbl.class == "factor") | (st.lbl.class == "character")) {
-      print("here4")
-      state.names <- 1:length(unique(as.vector(samples))) # We re-label the states numerically in this case to prevent possible re-ordering of the character names if we used unique instead
-      #state.names <- unique(as.vector(samples))
-    } else if((st.lbl.class == "numeric") | (st.lbl.class == "integer")) {
-      state.names <- 1:max(samples) # If the state labels are numeric, assume they are contiguous and ordered. **NOTE: this is a bad assumption with weird numbering schemes I could imagine may be useful in some cases; hence the warning.
-      #state.names <- unique(as.vector(samples))
-      warning("Assuming state names are numerical, contiguous and ordered. That's ok with you, right?")
-    } else {
-      stop("State labels must be factor, character, numeric or integer!")
-    }
-    #
-  } else {
-    state.names <- ordered.state.names
-  }
-  #print(node.names)
-  #print(state.names)
-
-  # if(is.null(node.names)){
-  #   tabl <- table(as.numeric(samples), as.numeric(gl(num.nodes, num.samps)), dnn=c("states", "nodes"))
-  # } else {
-  #   tabl <- table(samples, as.vector(sapply(1:num.nodes, function(xx){c(rep(node.names[xx], nrow(samples)))})), dnn=c("states", "nodes"))
-  #   #print(tabl)
-  # }
-
-  print(node.names)
-  tabl <- table(as.vector(samples), as.numeric(gl(num.nodes, num.samps)), dnn=c("states", "nodes"))
-  # Still reversing when using characters in samples. REPLACE ABOVE WITH ACTUAL MATRIX ELEMENTS.
-  # Do as a loop
-
-
-  print(node.names)
-  if(is.null(node.names)){
-    colnames(tabl) <- 1:num.nodes
-  } else {
-    colnames(tabl) <- node.names
-  }
-
-  # if(!is.null(state.names)){
-  #   rownames(tabl) <- state.names
-  # }
-
-  print(state.names)
-  rownames(tabl) <- state.names
-
-  print(addmargins(tabl))
-
-  # Bar colors should just alternate between green and red across the states. At some point make this all prettier.
-  barplot(tabl, ylab="Frequency", xlab="nodes", main="State Distributions", col=c("green", "red" ), beside=TRUE, width=.3)
-  #legend("right", title="States", legend= c(1,2), fill =c("turquoise4", "turquoise2"), box.lty=0)
-
-}
-
-
-#' @title       Plot marginal sample
-#' @description Plot the sample of states at each node
-#'
-#' @param samples The samples
-#'
-#' @details The function will plot the counts of each state at each node.
-#'
-#' @return Nothing.
-#'
-#' @examples XXXX
-#'
-#' @export
-plot.marginal.sample2 <- function(samples, crf) {
+plot.marginal.sample <- function(samples, crf) {
 
   num.nodes      <- crf$n.nodes
   num.samps      <- nrow(samples)
@@ -93,7 +20,21 @@ plot.marginal.sample2 <- function(samples, crf) {
   num.states.vec <- crf$n.states
 
   # Construct (marginal) table of state counts at each node:
-  all.state.names.vec <- unique(unlist(crf$node.state.names))
+  all.state.names.vec <- unique(unlist(state.names))
+
+  if(F %in% (sort(unique(as.vector(samples))) == sort(all.state.names.vec)) ) {
+    print("===============================")
+    print("State names in sample:")
+    print(sort(unique(as.vector(samples))))
+    print("===============================")
+    print("State names in crf object:")
+    print(sort(all.state.names.vec))
+    print("===============================")
+    stop("State names in sample must be the same as state names in crf object!")
+  }
+  #print( sort(unique(as.vector(samples))) == sort(all.state.names.vec) )
+  #print(F %in% sort(unique(as.vector(samples))) == sort(all.state.names.vec))
+
   tabl <- array(0, dim = c(length(all.state.names.vec), num.nodes) )
 
   for(i in 1:num.nodes){
@@ -114,8 +55,36 @@ plot.marginal.sample2 <- function(samples, crf) {
   #print(tabl)
 
   print(addmargins(tabl, margin = c(1)))
+  print(tabl/num.samps)
 
   # Bar colors should just alternate between green and red across the states. At some point make this all prettier.
   barplot(tabl, ylab="Frequency", xlab="nodes", main="State Distributions", col=c("green", "red" ), beside=TRUE, width=0.3)
+
+}
+
+
+#' @title       Plot configuration sample
+#' @description Plot the sample of configurations
+#'
+#' @param samples the samples
+#' @param samples a crf object
+#'
+#' @details The function will plot the counts of each unique configuration of states in a sample of configurations.
+#'
+#' @return Nothing.
+#'
+#' @examples XXXX
+#'
+#' @export
+plot.configuration.sample <- function(samples, num.top.configs=3) {
+
+  # Configuration (aggregated) frequency table
+  config.cnts <- as.data.frame(ftable(data.frame(samples)))
+  cnts        <- config.cnts[,ncol(config.cnts)]
+  plot(1:nrow(config.cnts), cnts, typ="h", ylab="counts", xlab="config #", ylim=c(0,max(cnts)))
+
+  print(config.cnts)
+  print(paste0("Top ", num.top.configs, " configs:"))
+  print(config.cnts[ order(cnts, decreasing = T)[1:num.top.configs], ])
 
 }
--- tests/plot_samps_test3a.R
+++ tests/plot_samps_test3a.R
@@ -6,6 +6,8 @@ adj   <- ug(grphf, result = "matrix")
 
 # Define node state space and configuration space:
 s1 <- "up"; s2 <- "dn"; s3 <- "eh"
+#s1 <- 1; s2 <- 2; s3 <- 3
+#s1 <- "1"; s2 <- "2"; s3 <- "3"
 
 m2n  <- make.empty.field(graph.eq             = grphf,
                          #num.states           = 2,
@@ -13,20 +15,23 @@ m2n  <- make.empty.field(graph.eq             = grphf,
                          num.states           = c(2,3),
                          #state.names          = rep(list(c(s1,s2)), nrow(adj)),
                          #state.names          = rep(list(c(s1,s2,s3)), nrow(adj)),
-                         state.names          = list(c(s1,s2), c(s1,s3,s2)),
+                         #state.names          = list(c(s1,s2), c(s1,s3,s2)),  # ** DON'T DO THIS WITH NUMBER NAME STATES. KEEP THEM IN NUMERICAL ORDER OF CAN RUN INTO PROBLEMS WITH CRF SAMPLER REORDERING THE STATES!
+                         state.names          = list(c(s1,s2), c(s1,s2,s3)),
                          parameterization.typ = "standard",
                          plotQ                = T)
 # Make up some random parameters and insert them into the model:
 #par.vec <- -log(c(0.5,1,1))       # Total independence and 50/50 on each node is c(1,1,1)
 #par.vec <- -log(c(1,1,1,1,1,1,1)) # Total independence and 50/50 on each node
-par.vec <- -log(c(0.25,0.5,0.75,1,1,1))
+par.vec <- -log(c(1.25,0.5,0.75, 1.1,1.5,0.7))
 insert.params.and.pots(m2n, params.samp = par.vec)
 dump.crf(m2n)
 
-# Raw sample (raw case list)
+
 infer.junction(m2n)
 set.seed(1)
-m2n.samp <- sample.crf(m2n, 1000, sample.junction, dress.sampleQ = T)
-plot.marginal.sample2(m2n.samp, m2n)
+m2n.samp <- sample.crf(m2n, 5000, sample.junction, dress.sampleQ = T)
+plot.marginal.sample(m2n.samp, m2n)
+t(infer.junction(m2n)$node.bel)
 
-#dimnames = list(c("states","nodes"))
+plot.configuration.sample(m2n.samp)
+
